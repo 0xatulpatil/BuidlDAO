fe739e29d1c13bfb46b125f1424b295cc676acdb
Atul Patil <atuluttampatil.1757@gmail.com>
2021-12-20 00:27:17 +0530

    🏗: Major Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,355 @@
+import { useEffect, useMemo, useState } from "react";
+
+// import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
+import { ethers } from "ethers";
+
+import { ThirdwebSDK } from "@3rdweb/sdk";
+
+const sdk = new ThirdwebSDK("rinkeby");
+
+
+const tokenModule = sdk.getTokenModule(
+  "0x40EC74613e8da0Ff961EdEb164156619a7bf584B"
+);
+
+const voteModule = sdk.getVoteModule(
+  "0xF3243D56AD9960Dfd95E5EAe1afAEEaC5eAf2E1D",
+);
+
+// We can grab a reference to our ERC-1155 contract.
+const bundleDropModule = sdk.getBundleDropModule(
+  "0xfe09B84b38BE2Da408bC18dc5A7752652C001A7C",
+);
+
 const App = () => {
-  return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
+
+  const {connectWallet, address, error, provider} = useWeb3();
+  console.log("👋 Address:", address);
+
+
+   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+   const [isClaiming, setIsClaiming] = useState(false);
+
+   const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
+
+   const [memberAddresses, setMemberAddresses] = useState([]);
+
+   const [proposals, setProposals] = useState([]);
+   const [isVoting, setIsVoting] = useState(false);
+   const [hasVoted, setHasVoted] = useState(false);
+
+
+
+   const shortenAddress = (str) => {
+     return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+   }
+
+   
+   
+   const signer = provider  ? provider.getSigner() : undefined;
+
+    useEffect(() => {
+
+    sdk.setProviderOrSigner(signer);
+  }, [signer]);
+
+
+  useEffect(()=>{
+     if(!hasClaimedNFT){
+       return;
+     }
+     
+      bundleDropModule
+    .getAllClaimerAddresses("0")
+    .then((addresess) => {
+      console.log("🚀 Members addresses", addresess)
+      setMemberAddresses(addresess);
+    })
+    .catch((err) => {
+      console.error("failed to get member list", err);
+    });
+
+   },[hasClaimedNFT])
+
+ 
+   useEffect(()=>{
+
+     if (!hasClaimedNFT) {
+    return;
+    }
+
+   tokenModule
+    .getAllHolderBalances()
+    .then((amounts) => {
+      console.log("👜 Amounts", amounts)
+      setMemberTokenAmounts(amounts);
+    })
+    .catch((err) => {
+      console.error("failed to get token amounts", err);
+    });
+
+   },[hasClaimedNFT]);
+
+   useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+  // A simple call to voteModule.getAll() to grab the proposals.
+  voteModule
+    .getAll()
+    .then((proposals) => {
+      // Set state!
+      setProposals(proposals);
+      console.log("🌈 Proposals:", proposals)
+    })
+    .catch((err) => {
+      console.error("failed to get proposals", err);
+    });
+}, [hasClaimedNFT]);
+
+
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+  if (!proposals.length) {
+    return;
+  }
+
+  // Check if the user has already voted on the first proposal.
+  voteModule
+    .hasVoted(proposals[0].proposalId, address)
+    .then((hasVoted) => {
+      setHasVoted(hasVoted);
+      console.log("🥵 User has already voted")
+    })
+    .catch((err) => {
+      console.error("failed to check if wallet has voted", err);
+    });
+}, [hasClaimedNFT, proposals, address]);
+
+
+   //combining the memberAddresses and memberTokenAmounts into a single array
+const memberList = useMemo(() => {
+  return memberAddresses.map((address) => {
+    return {
+      address,
+      tokenAmount: ethers.utils.formatUnits(
+        // If the address isn't in memberTokenAmounts, it means they don't
+        // hold any of our token.
+        memberTokenAmounts[address] || 0,
+        18,
+      ),
+    };
+  });
+}, [memberAddresses, memberTokenAmounts]);
+
+
+   useEffect(() => {
+     if(!address){
+       return ;
+     }
+
+     return bundleDropModule
+         .balanceOf(address, "0")
+         .then((balance)=>{
+           if(balance.gt(0)){
+             setHasClaimedNFT(true);
+             console.log("🌟 This user has a membershif NFT!")
+           }else{
+             setHasClaimedNFT(false);
+             console.log("😭 This user doesn't have a membership NFT")
+           }
+         })
+         .catch((err)=>{
+           setHasClaimedNFT(false);
+           console.error("failed to nft balance", error);
+         });
+   }, [address]);
+//if user hasn't connected their wallet
+if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to NarutoDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  
+ if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🦾 $BUIDL DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <h2>Active Proposals</h2>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                setIsVoting(true);
+
+                const votes = proposals.map((proposal) => {
+                  let voteResult = {
+                    proposalId: proposal.proposalId,
+                    //abstain by default
+                    vote: 2,
+                  };
+                  proposal.votes.forEach((vote) => {
+                    const elem = document.getElementById(
+                      proposal.proposalId + "-" + vote.type
+                    );
+
+                    if (elem.checked) {
+                      voteResult.vote = vote.type;
+                      return;
+                    }
+                  });
+                  return voteResult;
+                });
+
+                try {
+                  const delegation = await tokenModule.getDelegationOf(address);
+                  if (delegation === ethers.constants.AddressZero) {
+                    await tokenModule.delegateTo(address);
+                  }
+                  try {
+                    await Promise.all(
+                      votes.map(async (vote) => {
+                        const proposal = await voteModule.get(vote.proposalId);
+                        if (proposal.state === 1) {
+                          return voteModule.vote(vote.proposalId, vote.vote);
+                        }
+                        return;
+                      })
+                    );
+                    try {
+                      await Promise.all(
+                        votes.map(async (vote) => {
+                          const proposal = await voteModule.get(
+                            vote.proposalId
+                          );
+
+                          if (proposal.state === 4) {
+                            return voteModule.execute(vote.proposalId);
+                          }
+                        })
+                      );
+                      setHasVoted(true);
+                      console.log("successfully voted");
+                    } catch (err) {
+                      console.error("failed to execute votes", err);
+                    }
+                  } catch (err) {
+                    console.error("failed to vote", err);
+                  }
+                } catch (err) {
+                  console.error("failed to delegate tokens");
+                } finally {
+                  setIsVoting(false);
+                }
+              }}
+            >
+              {proposals.map((proposal, index) => (
+                <div key={proposal.proposalId} className="card">
+                  <h5>{proposal.description}</h5>
+                  <div>
+                    {proposal.votes.map((vote) => (
+                      <div key={vote.type}>
+                        <input
+                          type="radio"
+                          id={proposal.proposalId + "-" + vote.type}
+                          name={proposal.proposalId}
+                          value={vote.type}
+                          //default the "abstain" vote to chedked
+                          defaultChecked={vote.type === 2}
+                        />
+                        <label htmlFor={proposal.proposalId + "-" + vote.type}>
+                          {vote.label}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+              <button disabled={isVoting || hasVoted} type="submit">
+                {isVoting
+                  ? "Voting..."
+                  : hasVoted
+                    ? "You Already Voted"
+                    : "Submit Votes"}
+              </button>
+              <small>
+                This will trigger multiple transactions that you will need to
+                sign.
+              </small>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+
+   const mintNft = () => {
+    setIsClaiming(true);
+    // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
+    bundleDropModule
+    .claim("0", 1)
+    .catch((err) => {
+      console.error("failed to claim", err);
+      setIsClaiming(false);
+    })
+    .finally(() => {
+      // Stop loading state.
+      setIsClaiming(false);
+      // Set claim state.
+      setHasClaimedNFT(true);
+      // Show user their fancy new NFT!
+      console.log(
+        `Successfully Minted! Check it our on OpenSea: https://testnets.opensea.io/assets/${bundleDropModule.address}/0`
+      );
+    });
+  }
+
+   return (
+    <div className="mint-nft">
+      <h1>Mint your free 🍪DAO Membership NFT</h1>
+      <button
+        disabled={isClaiming}
+        onClick={() => mintNft()}
+      >
+        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+      </button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// 4 = Rinkeby.
+const supportedChainIds = [4];
+
+const connectors = {
+  injected: {},
+};
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <div className="landing">
+        <App />
+      </div>
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
