b24e338a37267983eacd2c7fee9918c9da6c05ff
Atul Patil <atuluttampatil.1757@gmail.com>
2021-12-20 00:46:20 +0530

    😊

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,7 @@ const memberList = useMemo(() => {
 if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to BuidlDAO</h1>
         <button onClick={() => connectWallet("injected")} className="btn-hero">
           Connect your wallet
         </button>
